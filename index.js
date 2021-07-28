502e22cdeaa854311368aeb86c688fde4a496816
adnanraghib1308 <adnan.raghib@gmail.com>
2021-07-28 22:38:24 +0530

    initial commit

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
 const apis = require('./routes');
+require("dotenv").config();
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use('/', apis);
 
-app.listen(3099, ()=> {
+app.listen(3099 || process.env.PORT, ()=> {
   console.log("app running");
 })
 
